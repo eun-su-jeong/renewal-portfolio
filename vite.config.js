b5e5985772e1bbbe66a3549e6d6aadded7d73367
eunjjang <83636742+eun-su-jeong@users.noreply.github.com>
2025-07-21 17:44:21 +0900

    fix: update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
 export default defineConfig({
-  base: '/Renewal_Portfolio/',
+  base: '/puble_portfolio/renewal/',
   plugins: [react()],
   resolve: {
     alias: {
