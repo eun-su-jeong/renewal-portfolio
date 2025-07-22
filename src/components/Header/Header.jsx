c761b637e1b3b2dbe8f2e2787a6f14be7663a274
eunjjang <83636742+eun-su-jeong@users.noreply.github.com>
2025-07-22 17:45:10 +0900

    feat: scroll feat: prevent background scrolling when mobile menu is open

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,6 +15,18 @@ const Header = () => {
 
     }, []);
 
+    useEffect(() => {
+        if (isMenuOpen) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'auto';
+        }
+
+        return () => {
+            document.body.style.overflow = 'auto';
+        }
+    }, [isMenuOpen]);
+
     return (
         <header className={styles.header}>
             <div className={styles.header__inner}>
